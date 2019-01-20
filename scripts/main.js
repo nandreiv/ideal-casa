2da1e0ea86cd4a807ea561132a3cd0fd11a3bac5
nandreiv <nedea.andreiv@gmail.com>
2019-01-20 18:19:34 +0200

    second commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,15 @@
-
 const anunturiList = document.querySelector('#anunturiContainer');
 const detaliiShow = document.querySelector('#detaliiContainer');
 const form = document.querySelector('#addAnunt');
 
 // Create elements and render
-function renderAnunturi(doc){
+function renderAnunturi(doc) {
 
   let an = doc.data().an;
   let camere = doc.data().camere;
   let descriere = doc.data().descriere;
   let pret = doc.data().pret;
   let suprafata = doc.data().suprafata;
-
 
   let str = `<div class="col s12 m6 l4 xl3" data-id='${doc.id}'>
               <div class="card medium">
@@ -28,43 +26,54 @@ function renderAnunturi(doc){
                   <p><i class="tiny material-icons">location_city</i> ${an}</p>
                 </div>
                 <div class="card-action">
-                  <a href="#" class='${doc.id}'>Detalii</a>
+                  <button class="btn wave-effect waves-light" onclick='showDetails(this)'>Detalii</button>
                 </div>
               </div>
             </div>`
 
-  anunturiList.insertAdjacentHTML('afterbegin',str);
+  anunturiList.insertAdjacentHTML('afterbegin', str);
 }
 
-function showDetails(doc) {
+function showDetails(elem) {
 
-  document.querySelector('#anunturiContainer').innerHTML = "";
+  let id = elem.parentElement.parentElement.parentElement.getAttribute('data-id');
+  let anunt = db.collection('anunturi').doc(id);
+  anunt.get().then(function(doc) {
+    if (doc.exists) {
+      document.querySelector('#anunturiContainer').innerHTML = "";
 
-  let an = doc.data().an;
-  let camere = doc.data().camere;
-  let descriere = doc.data().descriere;
-  let pret = doc.data().pret;
-  let suprafata = doc.data().suprafata;
+      let an = doc.data().an;
+      let camere = doc.data().camere;
+      let descriere = doc.data().descriere;
+      let pret = doc.data().pret;
+      let suprafata = doc.data().suprafata;
 
+      console.log(an, camere, descriere, pret, suprafata);
 
-  let str = `<div class="col s12">
-                <div class="image">
-                  <img src="img/flat2.jpg" class="imgCard">
-                </div>
-                <div class="content">
-                  <p>${descriere}</p>
-                </div>
-                <div id="detaliiImob">
-                  <p><i class="tiny material-icons">euro_symbol</i> ${pret}</p>
-                  <p><i class="tiny material-icons">aspect_ratio</i> ${suprafata} m²</p>
-                  <p><i class="tiny material-icons">local_hotel</i> ${camere}</p>
-                  <p><i class="tiny material-icons">location_city</i> ${an}</p>
-                </div>
-            </div>`
+      let str = `<div class="col s12 anunt">
+                    <div class="image">
+                      <img src="img/flat2.jpg" class="imgCard">
+                    </div>
+                    <div class="content">
+                      <p>${descriere}</p>
+                    </div>
+                    <div id="detaliiImob">
+                      <p><i class="tiny material-icons">euro_symbol</i> ${pret}</p>
+                      <p><i class="tiny material-icons">aspect_ratio</i> ${suprafata} m²</p>
+                      <p><i class="tiny material-icons">local_hotel</i> ${camere}</p>
+                      <p><i class="tiny material-icons">location_city</i> ${an}</p>
+                    </div>
+                </div>`
 
-  detaliiShow.insertAdjacentHTML('afterbegin',str);
-
-}
+      detaliiShow.insertAdjacentHTML('afterbegin', str);
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  }).catch(function(error) {
+    console.log("Error getting document:", error);
+  });
+};
 
 // Getting data from database
 db.collection('anunturi').get().then((snapshot) => {
@@ -74,34 +83,16 @@ db.collection('anunturi').get().then((snapshot) => {
 });
 
 // Showing setails for the card
-let targetNode = document.getElementById('anunturiContainer');
-let setup = { attributes: true, childList: true, subtree: true };
-var callback = function(mutationsList, observer) {
-    for(var mutation of mutationsList) {
-        if (mutation.type == 'childList') {
-            let detalii = document.querySelector('.card-action a').getAttribute('class');
-            db.collection('anunturi').doc(`${detalii}`).get().then((snapshot) => {
-              snapshot.doc.forEach(doc => {
-                showDetails(doc);
-              })
-            });
-        }
-        else if (mutation.type == 'attributes') {
-            console.log('The ' + mutation.attributeName + ' attribute was modified.');
-        }
-    }
-};
-var observer = new MutationObserver(callback);
-observer.observe(targetNode, setup);
+
 
 // Filtered render
 let searchFilter = document.querySelector('#searchFilter');
 
-searchFilter.addEventListener('click',(e) => {
+searchFilter.addEventListener('click', (e) => {
   let pret = document.querySelector('#filtruPret').value;
   let supraf = document.querySelector('#filtruSupraf').value;
   document.querySelector('#anunturiContainer').innerHTML = "";
-  db.collection('anunturi').where('pret', '<',`${pret}`).get().then((snapshot) => {
+  db.collection('anunturi').where('pret', '<', `${pret}`).get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
       renderAnunturi(doc);
     });
